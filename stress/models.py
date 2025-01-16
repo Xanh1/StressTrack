b080e1fe0426bff674a4c5051331241d86fe077f
Miguel Angel <apolo.miguel12x@gmail.com>
2025-01-15 22:23:31 -0500

    refactor: test model add created_at & update_at, and update test template

--- stress/models.py
+++ stress/models.py
@@ -83,8 +83,11 @@ class Task(models.Model):
 
 class Test(models.Model):
     title = models.CharField(max_length=255)
+    due_date = models.DateField(blank=True, null=True)
     Team = models.ManyToManyField(Team, related_name='tests')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='tests', default='2')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title}"
